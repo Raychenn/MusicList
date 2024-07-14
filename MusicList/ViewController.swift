ed09d40c7974e502911d9f2d489951fe13a769af
Raychenn <r0975929562@gmail.com>
2024-07-14 10:29:17 +0800

    Initial Commit

---
+++ MusicList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicList
+//
+//  Created by Boray Chen on 2024/7/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
