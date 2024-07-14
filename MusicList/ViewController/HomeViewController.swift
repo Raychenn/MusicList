e96d2ce0844fbaf72c06cad26c31a6c745662c91
Raychenn <r0975929562@gmail.com>
2024-07-14 16:06:43 +0800

    UICollectionViewCell UI setup

--- MusicList/ViewController/HomeViewController.swift
+++ MusicList/ViewController/HomeViewController.swift
@@ -9,23 +9,60 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    // MARK: - Properties
+    
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
+        layout.itemSize = UICollectionViewFlowLayout.automaticSize
+        layout.estimatedItemSize = CGSize(width: UIScreen.main.bounds.width, height: 100)
+        layout.scrollDirection = .vertical
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.showsVerticalScrollIndicator = false
+        collectionView.alwaysBounceVertical = true
         collectionView.dataSource = self
         collectionView.delegate = self
-        collectionView.register(ThumbnailCourseCollectionViewCell.self, forCellWithReuseIdentifier: ThumbnailCourseCollectionViewCell.className())
+        collectionView.register(PlayListCell.self, forCellWithReuseIdentifier: PlayListCell.className())
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
         return collectionView
     }()
     
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        swtupUI()
     }
+    
+    // MARK: - Helpers
+    
+    private func swtupUI() {
+        view.backgroundColor = .white
+        collectionView.backgroundColor = .white
+        view.addSubview(collectionView)
+        collectionView.fillSuperview()
+    }
+}
 
+// MARK: - UICollectionViewDataSource
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let playListCell = collectionView.dequeueReusableCell(withReuseIdentifier: PlayListCell.className(), for: indexPath) as? PlayListCell else {
+            return UICollectionViewCell()
+        }
+        
+        
+        return playListCell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension HomeViewController: UICollectionViewDelegate {
 
 }
 
--- MusicList/Views/PlayListCell.swift
+++ MusicList/Views/PlayListCell.swift
@@ -5,4 +5,113 @@
 //  Created by Boray Chen on 2024/7/14.
 //
 
-import Foundation
+import UIKit
+
+class PlayListCell: UICollectionViewCell {
+    
+    // MARK: - Initial
+    
+    private let artworkImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(resource: .artwork)
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .lightGray
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 5
+        return imageView
+    }()
+    
+    private let trackNameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 14, weight: .semibold)
+        label.text = "long track name long long long longlonglonglonlong track name long long long longlonglonglonlong track name long long long longlonglonglon"
+        return label
+    }()
+    
+    private let trackTimeLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        label.text = "4:15"
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        label.text = "long description:  here is description, pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long. pretty long."
+        return label
+    }()
+    
+    // MARK: - Playing
+    
+    private let shortTrackNameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 14, weight: .semibold)
+        return label
+    }()
+    
+    private let playStatusLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        layoutIfNeeded()
+        let size = systemLayoutSizeFitting(layoutAttributes.size)
+        let frame = CGRect(origin: layoutAttributes.frame.origin, size: CGSize(width: size.width, height: size.height))
+        layoutAttributes.frame = frame
+        return layoutAttributes
+    }
+    
+    private func setupUI() {
+        backgroundColor = .red
+        contentView.addSubview(trackNameLabel)
+        trackNameLabel.anchor(top: contentView.topAnchor,
+                              left: contentView.leadingAnchor,
+                          paddingTop: 22,
+                          paddingLeft: 138)
+        
+        contentView.addSubview(trackTimeLabel)
+        trackTimeLabel.centerYAnchor.constraint(equalTo: trackNameLabel.centerYAnchor).isActive = true
+        trackTimeLabel.anchor(left: trackNameLabel.trailingAnchor,
+                              right: contentView.trailingAnchor,
+                              paddingLeft: 10,
+                              paddingRight: 40)
+        
+        contentView.addSubview(artworkImageView)
+        artworkImageView.setDimensions(height: 100, width: 100)
+        // top: 50, left: 10
+        artworkImageView.anchor(top: trackNameLabel.bottomAnchor,
+                                left: contentView.leadingAnchor,
+                                paddingTop: 10,
+                                paddingLeft: 10)
+    
+        contentView.addSubview(descriptionLabel)
+        descriptionLabel.anchor(top: artworkImageView.topAnchor,
+                                left: artworkImageView.trailingAnchor,
+                                bottom: contentView.bottomAnchor,
+                                right: contentView.trailingAnchor,
+                                paddingLeft: 18,
+                                paddingRight: 10)
+    }
+}
